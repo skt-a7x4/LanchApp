7f324b340c224d074275eebd075b07f6ed679cf8
skt-a7x4 <ta6339@icloud.com>
2022-04-23 14:07:10 +0900

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lanch App
+//
+//  Created by 酒匂竜也 on 2022/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
